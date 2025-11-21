84ace0b3364d0d78f161cd520fddda6e35bc919d
alexdev <alexdev.workenv@gmail.com>
2025-11-21 15:46:43 -0500

    feat(app): innit commit

---
+++ src/test/java/com/backend/demojwtauth/app/DemoJwtAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.demojwtauth.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJwtAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
